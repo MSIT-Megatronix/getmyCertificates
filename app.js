512bac308b7eebf37f4094a44e6e507927bf91b8
Shuvam002 <senshuvam.12047@gmail.com>
2024-03-30 11:00:29 +0530

    certificate Generator

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-// Authors: Ayush Chakraborty, Samadrita Saha
-// Year of Contribution: 2022
+// Authors: Ayush Chakraborty, Samadrita Saha, Shuvam Sen
+// Year of Contribution: 2024
 // Organization : Megatronix
 // Project Title: Certificate Generator
 
@@ -12,26 +12,33 @@ const express = require('express')
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
-
+app.use('/public', express.static('public'))
 const ejs = require('ejs');
 const textOverlay = require('./CertificateGenerator');
+const fs = require('fs');
 app.set('view engine', 'ejs');
 
 const port = 3000
 app.get('/',(req,res)=>{
     res.render('home')
 })
-app.post('/text',(req,res)=>{
-    //const name=req.body
+app.post('/text.ejs',async (req,res)=>{
     const json=req.body.Name
-    //console.log(req.body.Name)
-    res.end()
-    //console.log(req.body.name)
     const j=JSON.stringify(json)
-    textOverlay(JSON.parse(j))
-
+    res.render('text') // Send the response
+    textOverlay(JSON.parse(j)) // Do additional processing
 })
 
+app.get('/text.ejs', async (req, res) => {
+    try {
+        let files = fs.readdirSync('./public/exports');
+        console.log(files);
+        res.json(files);
+    } catch (err) {
+        console.error('Error reading directory:', err);
+        res.status(500).send(err);
+    }
+});
 
 app.listen(port,()=>{
     console.log("Server Started at port= "+port)
